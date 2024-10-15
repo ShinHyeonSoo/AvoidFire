12ce0c5cfed31c863e411c9bd908942e42bacff9
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-10-15 20:19:04 +0900

    Update

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fire : MonoBehaviour
+public class TestFire : MonoBehaviour
 {
     public GameObject fireobj;
     float size;
