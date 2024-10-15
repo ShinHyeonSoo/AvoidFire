baad0ba633bb4e6a0e70da2639c9c33721ec6540
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-10-15 11:43:55 +0900

    Init
    프로젝트 세팅

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager _instance;
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
