4c3a972dc284d6d9957d63c2cf90104e1276134d
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-10-15 13:49:29 +0900

    Add Fonts, GameManager, Utils
    Fonts, GameManager, Utils 추가

---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.SceneManagement;
+
+public enum SceneNames 
+{ 
+    Title = 0, 
+    Game, 
+}
+
+public static class Utils
+{
+    public static string GetActiveScene()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    public static void LoadScene(string sceneName = "")
+    {
+        if("" == sceneName)
+        {
+            SceneManager.LoadSceneAsync(GetActiveScene());
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+
+    public static void LoadScene(SceneNames sceneName)
+    {
+        // SceneNames 열거형으로 매개변수를 받아온 경우 ToString() 처리
+        SceneManager.LoadSceneAsync(sceneName.ToString());
+    }
+}
