c01474a890cc08574699123c26a914777ac7001b
김홍섭 <qufrj7@naver.com>
2024-10-16 19:54:39 +0900

    [add]score

--- Assets/Scripts/Collision/FireCollision.cs
+++ Assets/Scripts/Collision/FireCollision.cs
@@ -10,7 +10,6 @@ public class FireCollision : MonoBehaviour
     // 플레이어의 하트가 0개가 되었을 경우: 플레이어가 사망하고 게임이 종료된다.
     // 불과 바닥이 만났을 경우 : 불이 파괴된다.
     private Rigidbody2D rigidbody2D;
-    private int heart = 3; // 플레이어의 초기 하트 수
 
     public void Start() 
     { 
@@ -19,30 +18,16 @@ public class FireCollision : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
         if (collision.gameObject.CompareTag("Fire"))
         {
-            heart--;
-            // 1. 플레이어가 넉백된다.(0.1~0.5초 정도 경직)
-            // 2. 플레이어가 맞아서 하트가 감소했다는 리액션(문구 등)
-            if (heart <= 0)
-            {
-                Die();
-            }
-        }
-
-        if (collision.gameObject.CompareTag("Ground"))
-        {
+            player.TakeDamage();
             Destroy(this.gameObject);
         }
 
-        if (collision.gameObject.CompareTag("Player"))
+        else if (collision.gameObject.CompareTag("Ground"))
         {
             Destroy(this.gameObject);
-        }
-
-        void Die()
-        {
-            Debug.Log("플레이어가 사망하였습니다.");
         }
     }
 }
