5e2ee977205bd1d56232edba46a4883cb147a534
김홍섭 <qufrj7@naver.com>
2024-10-17 19:36:20 +0900

    [fix]collision, score

--- Assets/Scripts/Collision/FireCollision.cs
+++ Assets/Scripts/Collision/FireCollision.cs
@@ -4,31 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireCollision : MonoBehaviour
-{   
-    // 충돌이 필요한 객체: 플레이어와 불
-    // 충돌이 일어났을 경우: 플레이어의 하트 감소
-    // 플레이어의 하트가 0개가 되었을 경우: 플레이어가 사망하고 게임이 종료된다.
-    // 불과 바닥이 만났을 경우 : 불이 파괴된다.
-    private Rigidbody2D rigidbody2D;
-    private CircleCollider2D circleCollider2D;
-    int score = 1;
+{
+    private int groundTagHash;
+    private int playerTagHash;
 
-    public void Start() 
-    { 
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        circleCollider2D = GetComponent<CircleCollider2D>();
+    private void Awake()
+    {
+        groundTagHash = "Ground".GetHashCode();
+        playerTagHash = "Player".GetHashCode();
     }
+
+    int score = 1;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-        if (collision.gameObject.CompareTag("Player"))
+        int collisionTagHash = collision.gameObject.tag.GetHashCode();
+
+        if (collisionTagHash == playerTagHash)
         {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
             player.TakeDamage();
             Destroy(this.gameObject);
         }
 
-        else if (collision.gameObject.CompareTag("Ground"))
+        else if (collisionTagHash == groundTagHash)
         {
             Score.Instance.AddScore(score);
             Destroy(this.gameObject);
