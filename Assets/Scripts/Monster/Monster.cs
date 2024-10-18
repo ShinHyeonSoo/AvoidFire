fa46081fe78f11f6053e94004705980f89b99cb3
omyman12 <qkrrlcks00@gmail.com>
2024-10-18 20:01:30 +0900

    monster fire collision

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -5,10 +5,14 @@ public class Monster : MonoBehaviour
 {
     private float minX = -8.3f;
     private float maxX = 8.3f;
+    float knockback = 3; // 튕겨나갈 정도
+    float knockbackpower = 2; // 튕겨나갈 힘
+    public float speed;
 
     MonsterSpawn monsterSpawn;
 
     SpriteRenderer monsterRenderer;
+    Rigidbody2D rb2d;
 
     void Update()
     {
@@ -18,22 +22,31 @@ public class Monster : MonoBehaviour
     {
         monsterSpawn = GetComponentInParent<MonsterSpawn>();
         monsterRenderer = GetComponent<SpriteRenderer>();
-
+        rb2d = GetComponent<Rigidbody2D>();
         if (transform.position.x < minX)
         {
-            //monsterRenderer.flipX = false;
             gameObject.SetActive(false);
+            Score.Instance.AddScore(10);
         }
 
-        //몬스터를 뽑을때 flip되게 해야겟다 
         if (transform.position.x > maxX)
         {
-            //monsterRenderer.flipX = true;
             gameObject.SetActive(false);
+            Score.Instance.AddScore(10); // 점수올라가는지 확인
         }
         if (monsterRenderer.flipX)
-            transform.Translate(Vector3.left * monsterSpawn.speed * Time.deltaTime);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
 
-        else transform.Translate(Vector3.right * monsterSpawn.speed * Time.deltaTime);
+        else transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            float dirX = transform.position.x - collision.transform.position.x > 0 ? (knockback) : -(knockbackpower);
+            rb2d.AddForce(new Vector2(dirX, (knockback)) * (knockbackpower), ForceMode2D.Impulse);
+        }
     }
 }
--- Assets/Scripts/Monster/MonsterSpawn.cs
+++ Assets/Scripts/Monster/MonsterSpawn.cs
@@ -10,19 +10,21 @@ public class MonsterSpawn : MonoBehaviour
 
     float minX = -8f;
     float maxX = 8f;
-    public int speed;
 
     private void Start()
     {
-        InvokeRepeating("SpawnMonster", 0f, 5f);
+        InvokeRepeating("SpawnMonster", 0f, 7f);
     }
 
     public void SpawnMonster()
     {
         string tag = Random.Range(0, 2) == 0 ? "zombiePrefab" : "skeletonPrefab";
-        GameObject monsterSpawn = objectPool.SpawnFromPool(tag);
-        SpriteRenderer spriteRenderer = monsterSpawn.GetComponent<SpriteRenderer>();
-        speed = Random.Range(1, 10);
+
+        GameObject monsterspawn = objectPool.SpawnFromPool(tag);
+        SpriteRenderer spriteRenderer = monsterspawn.GetComponent<SpriteRenderer>();
+
+        Monster monster = monsterspawn.GetComponent<Monster>();
+        monster.speed = Random.Range(1, 6);
         int randomX = Random.Range(0, 2);
         float spawnX = randomX == 0 ? minX : maxX;
 
@@ -32,6 +34,6 @@ public class MonsterSpawn : MonoBehaviour
         float spawnY = -2f;
 
         // 몬스터 위치 설정
-        monsterSpawn.transform.position = new Vector2(spawnX, spawnY);
+        monsterspawn.transform.position = new Vector2(spawnX, spawnY);
     }
 }
