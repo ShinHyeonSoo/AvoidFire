0ea103a34509ccded5b0943ca97aa2b7df43cf73
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-10-20 00:24:05 +0900

    Update TweenScreen
    TweenScreen 몬스터 효과 추가

--- Assets/Scripts/UI/Tween/TweenScreen.cs
+++ Assets/Scripts/UI/Tween/TweenScreen.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class TweenScreen : MonoBehaviour
 {
     private RectTransform _rectTrans;
+    [SerializeField]private RectTransform _rectTransMon;
+    [SerializeField] private Animator _animatorMon;
 
     private float _targetPosX = 0;
     private float _duration = 1f;
@@ -17,16 +19,19 @@ public class TweenScreen : MonoBehaviour
         DOTween.Init();
         _rectTrans = GetComponent<RectTransform>();
 
+        var mon = transform.GetChild(0).gameObject;
+        _rectTransMon = mon.GetComponent<RectTransform>();
+        _animatorMon = mon.GetComponent<Animator>();
+
         Open();
     }
 
     public void Close(string sceneName)
     {
         var seq = DOTween.Sequence();
-
-        seq.Append(_rectTrans.DOAnchorPosX(_targetPosX, _duration).SetEase(Ease.OutCirc));
-
-        seq.Play().SetUpdate(true).OnComplete(() => { SceneManager.LoadScene(sceneName); });
+        
+        seq.Append(_rectTransMon.DOAnchorPosY(_targetPosX, _duration * 1.2f).SetEase(Ease.OutCirc));
+        seq.Play().SetUpdate(true).OnComplete(() => { LoadScene(sceneName); });
     }
 
     public void Open()
@@ -37,5 +42,13 @@ public class TweenScreen : MonoBehaviour
         seq.Append(_rectTrans.DOAnchorPosX(-1920, _duration * 0.5f));
 
         seq.Play().SetUpdate(true);
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        _animatorMon.SetTrigger("Start");
+        var seq = DOTween.Sequence();
+        seq.Append(_rectTrans.DOAnchorPosX(_targetPosX, _duration * 1.5f).SetEase(Ease.OutCirc));
+        seq.Play().SetUpdate(true).OnComplete(() => { SceneManager.LoadScene(sceneName); });
     }
 }
