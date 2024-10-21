5420b61cda5dea0623a63d61cb4cab398d115137
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-21 16:00:49 +0900

    feat(Player) : 캐릭터 선택음 추가

--- Assets/Scripts/UI/Button/CharacterSelectButton.cs
+++ Assets/Scripts/UI/Button/CharacterSelectButton.cs
@@ -8,5 +8,6 @@ public class CharacterSelectButton : MonoBehaviour
     public void OnClickButtonCharacterSelect(int index)
     {
         PlayerInformManager.instance.SetId(index);
+        SoundManager.Instance.Play("select", Sound.Sfx, 0.5f);
     }
 }
